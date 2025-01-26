1cde8a6aa38b0ac19377e1bd442766b33da9fb99
Badhon-Roy <roybadhon286@gmail.com>
2025-01-27 00:53:23 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+       <Button className="text-3xl text-red-200">Click me</Button>
+       <h2 className="text-3xl text-red-600">Hi There</h2>
+    </>
+  )
+}
+
+export default App
